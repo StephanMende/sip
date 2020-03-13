55aba325c68de5a21650552c943f5dde05673a80
Julien Hofer <hoferj@uni-hildesheim.de>
2020-03-13 18:35:55 +0100

    Text für Studiengang und alphabetische Reihenfolge

---
+++ web/modules/custom/berufliche_vorbildung/src/Form/BeruflicheVorbildungConfigForm.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\berufliche_vorbildung\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class BeruflicheVorbildungConfigForm extends ConfigFormBase {
+    const SETTINGS = 'berufliche_vorbildung.settings';
+
+    public function getFormId()
+    {
+        return 'berufliche_vorbildung_settings';
+    }
+
+    protected function getEditableConfigNames()
+    {
+        return [
+            static::SETTINGS,
+        ];
+    }
+
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        $config = $this->config(static::SETTINGS);
+
+        $form['berufliche_vorbildung_studiengang_text'] = [
+            '#type' => 'textarea',
+            '#title' => $this->t('Text für den Studiengang'),
+            '#default_value' => $config->get('berufliche_vorbildung_studiengang_text'),
+        ];
+
+        return parent::buildForm($form, $form_state);
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        $this->configFactory->getEditable(static::SETTINGS)
+            ->set('berufliche_vorbildung_studiengang_text', $form_state->getValue('berufliche_vorbildung_studiengang_text'))
+            ->save();
+
+        parent::submitForm($form, $form_state);
+    }
+}
